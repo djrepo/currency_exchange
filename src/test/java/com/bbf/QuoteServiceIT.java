58a84f112efe5dca02441aff1283c2324ea8fb00
Dalibor <dalibor.jacko@gmail.com>
2024-06-10 19:16:15 +0200

    reverting

--- src/test/java/com/bbf/QuoteServiceIT.java
+++ src/test/java/com/bbf/QuoteServiceIT.java
@@ -18,8 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -37,7 +35,7 @@ import java.util.List;
 //@UsingDataSet("init-database.xml")
 public class QuoteServiceIT {
 
-    @Deployment(testable = false)
+    @Deployment
     public static WebArchive createDeploymentPackage() {
         return ShrinkWrap.create(WebArchive.class, "test.war")
                 .addPackage(QuoteEntity.class.getPackage())
@@ -46,14 +44,9 @@ public class QuoteServiceIT {
                 .addPackage(ReadQuoteRequest.class.getPackage())
                 .addClasses( QuoteResource.class, IQuoteService.class, QuoteService.class, IQuoteRepository.class, QuoteRepository.class,JaxRsActivator.class)
                 .addAsResource("test-persistence.xml", "META-INF/persistence.xml")
-                //.addAsWebInfResource("beans.xml", "META-INF/beans.xml")
+                .addAsWebInfResource("test-beans.xml", "beans.xml")
                 .addAsWebInfResource("arquillian-ds.xml")
-                .addAsResource("META-INF/microprofile-config.properties")
-                //.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-                .addAsWebInfResource(new StringAsset("<beans xmlns=\"https://jakarta.ee/xml/ns/jakartaee\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
-                + "xsi:schemaLocation=\"https://jakarta.ee/xml/ns/jakartaee https://jakarta.ee/xml/ns/jakartaee/beans_3_0.xsd\"\n"
-                + "bean-discovery-mode=\"all\">\n"
-                + "</beans>"), "beans.xml");
+                .addAsResource("META-INF/microprofile-config.properties");
     }
 
     @Inject
@@ -61,8 +54,7 @@ public class QuoteServiceIT {
 
     @Test
     public void testService() {
-        Path dumpPath = Path.of("src","test", "resources", "dump", "euraud.json");
-        log.info("dumpPath found "+dumpPath.toFile().exists());
+        Path dumpPath = Path.of("src","test", "resources", "dump", "euraudOneLine.json");
         try (InputStream is = new FileInputStream(dumpPath.toFile())){
             int result = service.importToDb(is);
             assertEquals(1, result);
