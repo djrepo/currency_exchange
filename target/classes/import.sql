ee7d695abf0b8533c4e3c5c55b449ea39aefd5e1
Dalibor <dalibor.jacko@gmail.com>
2024-06-11 20:37:45 +0200

    cleaning

--- target/classes/import.sql
+++
@@ -1,2 +0,0 @@
--- You can use this file to load seed data into the database using SQL statements
-insert into QUOTE_ENTITY (ID, ASK,ASK_VOLUME,BID,BID_VOLUME,TIME) values (0, 1563910,500,0,0,1533723600198000000)
